9d46233480c31b1d420937871bae9ed64a18656f
AIT M'BAREK ACHRAF <63782686+achrafaitmbarek@users.noreply.github.com>
2021-03-17 17:24:51 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace puzzle_game_2
+{
+    partial class puzzle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Label();
+            this.btn3 = new System.Windows.Forms.Label();
+            this.btn4 = new System.Windows.Forms.Label();
+            this.btn5 = new System.Windows.Forms.Label();
+            this.btn6 = new System.Windows.Forms.Label();
+            this.btn7 = new System.Windows.Forms.Label();
+            this.btn8 = new System.Windows.Forms.Label();
+            this.btnBlank = new System.Windows.Forms.Label();
+            this.lblNumber_mouvement = new System.Windows.Forms.Label();
+            this.New_try = new System.Windows.Forms.Button();
+            this._Stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Time_viewer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.AutoSize = true;
+            this.btn1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn1.Location = new System.Drawing.Point(12, 13);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(83, 91);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            // 
+            // btn2
+            // 
+            this.btn2.AutoSize = true;
+            this.btn2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
+            this.btn2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn2.Location = new System.Drawing.Point(100, 13);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(83, 91);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            // 
+            // btn3
+            // 
+            this.btn3.AutoSize = true;
+            this.btn3.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
+            this.btn3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn3.Location = new System.Drawing.Point(189, 13);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(83, 91);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "3";
+            // 
+            // btn4
+            // 
+            this.btn4.AutoSize = true;
+            this.btn4.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
+            this.btn4.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn4.Location = new System.Drawing.Point(11, 108);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(83, 91);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "4";
+            // 
+            // btn5
+            // 
+            this.btn5.AutoSize = true;
+            this.btn5.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
+            this.btn5.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn5.Location = new System.Drawing.Point(100, 108);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(83, 91);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "5";
+            // 
+            // btn6
+            // 
+            this.btn6.AutoSize = true;
+            this.btn6.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
+            this.btn6.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn6.Location = new System.Drawing.Point(189, 108);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(83, 91);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "6";
+            // 
+            // btn7
+            // 
+            this.btn7.AutoSize = true;
+            this.btn7.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
+            this.btn7.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn7.Location = new System.Drawing.Point(11, 204);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(83, 91);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "7";
+            // 
+            // btn8
+            // 
+            this.btn8.AutoSize = true;
+            this.btn8.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
+            this.btn8.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn8.Location = new System.Drawing.Point(100, 204);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(83, 91);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "8";
+            // 
+            // btnBlank
+            // 
+            this.btnBlank.AutoSize = true;
+            this.btnBlank.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnBlank.Location = new System.Drawing.Point(209, 272);
+            this.btnBlank.Name = "btnBlank";
+            this.btnBlank.Size = new System.Drawing.Size(0, 13);
+            this.btnBlank.TabIndex = 8;
+            // 
+            // lblNumber_mouvement
+            // 
+            this.lblNumber_mouvement.AutoSize = true;
+            this.lblNumber_mouvement.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblNumber_mouvement.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber_mouvement.Location = new System.Drawing.Point(8, 311);
+            this.lblNumber_mouvement.Name = "lblNumber_mouvement";
+            this.lblNumber_mouvement.Size = new System.Drawing.Size(137, 18);
+            this.lblNumber_mouvement.TabIndex = 9;
+            this.lblNumber_mouvement.Text = "touch the to start";
+            // 
+            // New_try
+            // 
+            this.New_try.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.New_try.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.New_try.Location = new System.Drawing.Point(367, 120);
+            this.New_try.Name = "New_try";
+            this.New_try.Size = new System.Drawing.Size(118, 28);
+            this.New_try.TabIndex = 10;
+            this.New_try.Text = "New Try";
+            this.New_try.UseVisualStyleBackColor = false;
+            this.New_try.Click += new System.EventHandler(this.New_try_Click);
+            // 
+            // _Stop
+            // 
+            this._Stop.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this._Stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._Stop.Location = new System.Drawing.Point(367, 163);
+            this._Stop.Name = "_Stop";
+            this._Stop.Size = new System.Drawing.Size(118, 28);
+            this._Stop.TabIndex = 11;
+            this._Stop.Text = "Stop Game";
+            this._Stop.UseVisualStyleBackColor = false;
+            this._Stop.Click += new System.EventHandler(this._Stop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Time_viewer
+            // 
+            this.Time_viewer.AutoSize = true;
+            this.Time_viewer.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Time_viewer.Location = new System.Drawing.Point(364, 74);
+            this.Time_viewer.Name = "Time_viewer";
+            this.Time_viewer.Size = new System.Drawing.Size(54, 16);
+            this.Time_viewer.TabIndex = 12;
+            this.Time_viewer.Text = "Time :";
+            this.Time_viewer.Click += new System.EventHandler(this.Time_viewer_Click);
+            // 
+            // puzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Time_viewer);
+            this.Controls.Add(this._Stop);
+            this.Controls.Add(this.New_try);
+            this.Controls.Add(this.lblNumber_mouvement);
+            this.Controls.Add(this.btnBlank);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "puzzle";
+            this.Text = "PUZZLE GAME";
+            this.Load += new System.EventHandler(this.puzzle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label btn1;
+        private System.Windows.Forms.Label btn2;
+        private System.Windows.Forms.Label btn3;
+        private System.Windows.Forms.Label btn4;
+        private System.Windows.Forms.Label btn5;
+        private System.Windows.Forms.Label btn6;
+        private System.Windows.Forms.Label btn7;
+        private System.Windows.Forms.Label btn8;
+        private System.Windows.Forms.Label btnBlank;
+        private System.Windows.Forms.Label lblNumber_mouvement;
+        private System.Windows.Forms.Button New_try;
+        private System.Windows.Forms.Button _Stop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Time_viewer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzle_game_2
+{
+	public partial class puzzle : Form
+	{
+
+		int i;
+		static private int[,] buttons = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+		private int ar, ac;
+		private int Number_mouvement = 0;
+		public puzzle()
+		{
+			InitializeComponent();
+		}
+		public void r_andom()
+		{
+			ar = 2;
+			ac = 2;
+			btnBlank.Location = new Point(200, 200);
+			btnBlank.Size = new Size(90, 90);
+			lblNumber_mouvement.Font = new Font("bigger", 19);
+			lblNumber_mouvement.Size = new Size(350, 40);
+			//lblNumber_mouvement.Location = new Point(0, 1150);
+			this.MouseDown += new MouseEventHandler(Click_win);
+			this.KeyDown += new KeyEventHandler(KeyDown_win);
+			PlaceRandom();
+		}
+
+
+		private void puzzle_Load(object sender, EventArgs e)
+		{
+			r_andom();
+			_Stop.Enabled=false;
+			New_try.Enabled=false;
+
+		}
+		private void Click_win(Object sender, MouseEventArgs e)
+		{
+			Random rnd = new Random();
+
+			MessageBox.Show(rnd.Next(8).ToString());
+			MessageBox.Show("press out of the area ");
+		}
+
+
+		private void KeyDown_win(Object sender, KeyEventArgs k)
+		{
+			int help;
+			switch (k.KeyCode)
+			{
+	case Keys.Down:
+					if (ar > 0)
+					{
+						help = buttons[ar, ac];
+						buttons[ar, ac] = buttons[ar - 1, ac];
+						buttons[ar - 1, ac] = help;
+						Swap(buttons[ar, ac], help);
+						ar--;
+						Number_mouvement++;
+					}
+					break;
+
+	case Keys.Left:
+					if (ac < 2)
+					{
+						help = buttons[ar, ac];
+						buttons[ar, ac] = buttons[ar, ac + 1];
+						buttons[ar, ac + 1] = help;
+						Swap(buttons[ar, ac], help);
+						ac++;
+						Number_mouvement++;
+					}
+					break;
+	case Keys.Up:
+
+
+					if (ar < 2)
+					{
+						help = buttons[ar, ac];
+						buttons[ar, ac] = buttons[ar + 1, ac];
+						buttons[ar + 1, ac] = help;
+						Swap(buttons[ar, ac], help);
+						ar++;
+						Number_mouvement++;
+					}
+
+					break;
+	case Keys.Right:
+					if (ac > 0)
+					{	help = buttons[ar, ac];
+						buttons[ar, ac] = buttons[ar, ac - 1];
+						buttons[ar, ac - 1] = help;
+						Swap(buttons[ar, ac], help);
+						ac--;
+						Number_mouvement++;}
+					break;
+
+			}
+			lblNumber_mouvement.Text = "Number of mouvement  " + Number_mouvement.ToString();
+			Boolean same = true;
+			String empty = "";
+			for (int ctr = 1, i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++, ctr++)
+				{
+					if (buttons[i, j] != ctr)
+					{
+						same = false;
+						break;
+					}
+					empty = empty + "\t" + buttons[i, j];
+
+				}
+				empty = empty + "\n";
+			}
+
+			if (same && i<=300)
+			{
+				timer1.Stop();
+				MessageBox.Show("Great ! you've done in  " + i + " and Number_mouvement" + Number_mouvement);
+			}
+			else{
+				if (!same && i>300)
+	{
+					MessageBox.Show("you lose");
+					New_try.Enabled = true;
+					
+					i=0;
+					
+	}
+			}
+
+
+
+		}
+
+		private void Swap(int rotat_nm, int blank)
+		{
+			Point help;
+
+
+			switch (rotat_nm)
+			{
+				case 1:
+					help = btn1.Location;
+					btn1.Location = btnBlank.Location;
+					btnBlank.Location = help;
+					break;
+				case 2:
+					help = btn2.Location;
+					btn2.Location = btnBlank.Location;
+					btnBlank.Location = help;
+					break;
+				case 3:
+					help = btn3.Location;
+					btn3.Location = btnBlank.Location;
+					btnBlank.Location = help;
+					break;
+				case 4:
+					help = btn4.Location;
+					btn4.Location = btnBlank.Location;
+					btnBlank.Location = help;
+					break;
+				case 5:
+					help = btn5.Location;
+					btn5.Location = btnBlank.Location;
+					btnBlank.Location = help;
+					break;
+				case 6:
+					help = btn6.Location;
+					btn6.Location = btnBlank.Location;
+					btnBlank.Location = help;
+					break;
+				case 7:
+					help = btn7.Location;
+					btn7.Location = btnBlank.Location;
+					btnBlank.Location = help;
+					break;
+				case 8:
+					help = btn8.Location;
+					btn8.Location = btnBlank.Location;
+					btnBlank.Location = help;
+					break;
+			}
+
+		}
+
+		private void PlaceRandom()
+		{
+			int r, c;
+			r = 10; c = 10;
+			int i = 0;
+			ar = 0;
+			ac = 0;
+			Random rnd = new Random();
+			int val;
+			while (i < 8)
+			{
+				val = rnd.Next(9);
+				if (if_Exists(val) == true && val > 0)
+				{
+					buttons[ar, ac] = val;
+
+					switch (val)
+					{
+						case 1:
+							btn1.Location = new Point(c, r);
+
+							break;
+						case 2:
+							btn2.Location = new Point(c, r);
+
+							break;
+						case 3:
+							btn3.Location = new Point(c, r);
+
+							break;
+						case 4:
+							btn4.Location = new Point(c, r);
+
+							break;
+						case 5:
+							btn5.Location = new Point(c, r);
+
+							break;
+						case 6:
+							btn6.Location = new Point(c, r);
+
+							break;
+						case 7:
+							btn7.Location = new Point(c, r);
+
+							break;
+						case 8:
+							btn8.Location = new Point(c, r);
+
+							break;
+
+
+					}
+					c += 100;
+					ac++;
+					if (ac > 2)
+					{
+						ac = 0;
+						ar++;
+					}
+					if (c > 300)
+					{
+						c = 10;
+						r += 100;
+					}
+					i++;
+				}
+				else
+					continue;
+			}
+			btnBlank.Location = new Point(c, r);
+			buttons[2, 2] = 9;
+			
+		}
+
+        private void New_try_Click(object sender, EventArgs e)
+        {
+			New_try.Enabled = false;
+			_Stop.Enabled = true;
+		}
+
+        private void _Stop_Click(object sender, EventArgs e)
+        {
+			New_try.Enabled = true;
+			_Stop.Enabled = false;
+			
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                Time_viewer.Text = "00:00";
+            }
+		}
+
+        private void Time_viewer_Click(object sender, EventArgs e)
+        {
+			
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+			i++;
+			Time_viewer.Text = i.ToString() + " Seconds";
+        }
+
+        private Boolean if_Exists(int Num)
+		{
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 3; j++)
+				{
+
+					if (buttons[i, j] == Num)
+						return false;
+				}
+
+			return true;
+		}
+	}
+}
